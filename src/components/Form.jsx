bfe4b280bea840b406c777e5205ac1f87f4510cd
Daria <spodina.darya@gmail.com>
2023-10-18 15:10:01 +0200

    Image rendered to the page

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,38 +11,38 @@ function getRandomImage() {
   }
 
 return (
-    <div className="flex flex-row justify-around pt-8">
-  <div className="w-1/3">
-    <div className="flex flex-col lg:flex-row gap-2 pb-6">
-      <input
-        className="w-full rounded-md py-1 pl-3"
-        type="text"
-        id="input1"
-        placeholder="Top text"
-      />
-      <input
-        className="w-full rounded-md py-1 pl-3"
-        type="text"
-        id="input2"
-        placeholder="Bottom text"
-      />
+    <div className="flex flex-col lg:flex-row justify-around pt-8 pb-24">
+        <div className="w-1/3">
+            <div className="flex flex-col lg:flex-row gap-2 pb-6">
+            <input
+                className="w-full rounded-md py-1 pl-3"
+                type="text"
+                id="input1"
+                placeholder="Top text"
+            />
+            <input
+                className="w-full rounded-md py-1 pl-3"
+                type="text"
+                id="input2"
+                placeholder="Bottom text"
+            />
+            </div>
+            <div className="text-center bg-violet hover:bg-[#5C469C] rounded-lg">
+                <button
+                onClick={getRandomImage}
+                className="py-2 text-white text-md font-semibold"
+                > Get a new image
+                </button>
+            </div>
+        </div>
+        <div className="flex justify-center items-center bg-white box-border h-auto w-[500px] p-2">
+            <img
+                className="w-full h-full object-cover"
+                src={memeImage}
+                alt=""
+            />
+        </div>
     </div>
-    <div className="text-center bg-violet hover:bg-[#5C469C] rounded-lg">
-      <button
-        onClick={getRandomImage}
-        className="py-2 text-white text-md font-semibold"
-      > Get a new image
-      </button>
-    </div>
-  </div>
-  <div className="w-1/2 flex justify-center items-center">
-    <img
-      className="max-h-[500px] max-w-[800px]"
-      src={memeImage}
-      alt=""
-    />
-  </div>
-</div>
 
   );
 }
